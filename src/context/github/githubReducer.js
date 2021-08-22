5717b3f292c33f1216c2551a751ddfc2244c62c1
Srijeet <63194798+srijeet08@users.noreply.github.com>
2021-08-22 13:32:45 +0530

    removed warnings

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -1,6 +1,6 @@
-import {SEARCH_USERS, GET_USER, GET_REPOS, SET_LOADING, SET_ALERT, CLEAR_USERS} from '../types';
+import {SEARCH_USERS, GET_USER, GET_REPOS, SET_LOADING, CLEAR_USERS} from '../types';
 
-
+// eslint-disable-next-line
 export default (state, action) => {
   switch(action.type) {
     case SET_LOADING:
